ba0d8869e34bce3b953271619630caad8c11219c
quainof <fabiomjquaino@gmail.com>
2022-01-16 04:12:33 -0300

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,200 @@
+import React from 'react'
+import { Formik, Form, Field } from 'formik'
+import {useNavigate} from 'react-router-dom'
+import * as Yup from 'yup'
+import Alerta from './Alerta'
+import Spinner from './Spinner'
+
+
+const Formulario = ({cliente, cargando}) => {
+
+    const navigate = useNavigate()
+
+    const nuevoClienteSchema = Yup.object().shape({
+        nombre: Yup.string()
+                .min(3, 'El nombre es muy corto')
+                .max(40, 'El nombre es muy largo')
+                .required('El nombre del cliente es obligatorio'),
+        empresa: Yup.string()
+                .required('El nombre de la empresa es obligatorio'),
+        email: Yup.string()
+                .email('Email no válido')
+                .required('El Email es obligatorio'),
+        telefono: Yup.number()
+                .positive("El número no es válido")
+                .integer("El número no es válido")
+                .typeError("El número no es válido")
+    })
+
+    const handleSubmit = async (valores) =>{
+        try {
+            let respuesta
+            if(cliente.id) {
+
+                // Editar registro
+                const url = `http://localhost:4000/clientes/${cliente.id}`
+                respuesta = await fetch(url, {
+                    method: 'PUT',
+                    body: JSON.stringify(valores),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }) 
+
+            } else {
+
+                // Nuevo registro
+                const url = "http://localhost:4000/clientes"
+                respuesta = await fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify(valores),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+
+            }
+
+            const resultado = await respuesta.json() // o se puede poner solo await respuesta.json() sin usar resultado
+            console.log(resultado)
+            navigate('/clientes')
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        cargando ? <Spinner/> : (
+            <div className=' bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto'>
+                <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>
+                    {cliente?.nombre ? "Editar cliente" : "Agregar Cliente"}
+                </h1>
+                <Formik
+                    initialValues={{
+                        nombre: cliente?.nombre ?? "", // cliente.nombre ? cliente.nombre : ""
+                        empresa: cliente?.empresa ?? "",
+                        email: cliente?.email ?? "",
+                        telefono: cliente?.telefono ?? "",
+                        notas: cliente?.notas ?? ""
+                    }}
+                    enableReinitialize={true}
+                    onSubmit={ async (values, {resetForm}) => {
+                        await handleSubmit(values)
+                        resetForm()
+                    }}
+                    validationSchema={nuevoClienteSchema}
+                >
+                    { ({errors, touched}) => {
+                        
+                        return (
+                        <Form
+                            className='mt-10'
+                        >
+                            <div className='mb-4'>
+                                <label
+                                    className='text-gray-800'
+                                    htmlFor='nombre'
+                                    >Nombre:
+                                </label>
+                                <Field
+                                    id="nombre"
+                                    type="text"
+                                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-gray-500"
+                                    placeholder="Nombre del cliente"
+                                    name="nombre"
+                                />
+                                {errors.nombre && touched.nombre ? (
+                                    <Alerta>{errors.nombre}</Alerta>
+                                ) : null}
+                            </div>
+
+                            <div className='mb-4'>
+                                <label
+                                    className='text-gray-800'
+                                    htmlFor='empresa'
+                                    >Empresa:
+                                </label>
+                                <Field
+                                    id="empresa"
+                                    type="text"
+                                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-gray-500"
+                                    placeholder="Nombre de la empresa del cliente"
+                                    name="empresa"
+                                />
+                                {errors.empresa && touched.empresa ? (
+                                    <Alerta>{errors.empresa}</Alerta>
+                                ) : null}
+                            </div>
+
+                            <div className='mb-4'>
+                                <label
+                                    className='text-gray-800'
+                                    htmlFor='email'
+                                    >E-mail:
+                                </label>
+                                <Field
+                                    id="email"
+                                    type="email"
+                                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-gray-500"
+                                    placeholder="Email del cliente"
+                                    name="email"
+                                />
+                                {errors.email && touched.email ? (
+                                    <Alerta>{errors.email}</Alerta>
+                                ) : null}
+                            </div>
+
+                            <div className='mb-4'>
+                                <label
+                                    className='text-gray-800'
+                                    htmlFor='telefono'
+                                    >Teléfono:
+                                </label>
+                                <Field
+                                    id="telefono"
+                                    type="tel"
+                                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-gray-500"
+                                    placeholder="Teléfono del cliente"
+                                    name="telefono"
+                                />
+                                {errors.telefono && touched.telefono ? (
+                                    <Alerta>{errors.telefono}</Alerta>
+                                ) : null}
+                            </div>
+
+                            <div className='mb-4'>
+                                <label
+                                    className='text-gray-800'
+                                    htmlFor='notas'
+                                    >Notas:
+                                </label>
+                                <Field
+                                    as="textarea"
+                                    id="notas"
+                                    type="text"
+                                    className="mt-2 block w-full p-3 bg-gray-50 border-2 border-gray-500 h-40"
+                                    placeholder="Notas del cliente"
+                                    name="notas"
+                                />
+                            </div>
+
+                            <input 
+                                type="submit" 
+                                value={cliente?.nombre ? "Editar cliente" : "Agregar Cliente"}
+                                className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg'
+                            />
+                        </Form>
+                    )}}
+                </Formik>
+            </div>
+        )
+    )
+}
+
+Formulario.defaultProps = {
+    cliente: {},
+    cargando: false
+}
+
+export default Formulario
